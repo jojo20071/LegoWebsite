1e84fec4735b79d3fba65dbb2769ff29d831875f
jojo20071 <johanneshaederle18092@gmail.com>
2023-03-02 13:02:15 +0100

    update

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 //3. Optional: put the register button on bottom -20px
 
 function redirect(){
-    window.location.href="http://127.0.0.1:5500/registrieren";
+    window.location.href="http://127.0.0.1:5500/anmelden";
 }
