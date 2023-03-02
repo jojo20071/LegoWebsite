34e68c55ecc8593f1474aa52d8a817f0e60f4965
jojo20071 <johanneshaederle18092@gmail.com>
2023-03-02 12:21:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//1.center the input fields
+//2. Fonts
+//3. Optional: put the register button on bottom -20px
+
+function redirect(){
+    window.location.href="http://127.0.0.1:5500/registrieren";
+}
