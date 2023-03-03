efb4c91d11cadcd54ac26fa46e4eec96fafe3e87
jojo20071 <johanneshaederle18092@gmail.com>
2023-03-03 19:56:05 +0100

    d

--- home/script.js
+++ home/script.js
@@ -1,5 +1,7 @@
 function redirect(){
-    //window.location.href="http://127.0.0.1:5500/create";}/
+    //window.location.href="http://127.0.0.1:5500/create";
+
+}
 function on(){
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://192.168.178.25/relay/0?turn=toggle');
